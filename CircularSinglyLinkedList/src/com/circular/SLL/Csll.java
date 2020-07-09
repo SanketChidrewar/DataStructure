a526494558acca2e4e27157f65b360ebc9da1d8e
SanketChidrewar <sanketchidrewar11@gmail.com>
2020-07-09 16:18:22 +0530

    Added code to insert new node in CSLL at beginning, end and also to remove node at beginning

--- CircularSinglyLinkedList/src/com/circular/SLL/Csll.java
+++ CircularSinglyLinkedList/src/com/circular/SLL/Csll.java
@@ -1,5 +1,7 @@
 package com.circular.SLL;
 
+import java.util.NoSuchElementException;
+
 public class Csll {
 
 	private ListNode last;
@@ -60,7 +62,7 @@ public class Csll {
 		ListNode first = last.next;
 		ListNode temp = last.next;
 		
-		System.out.print(temp.data + "-->");
+		System.out.print("\n"+temp.data + "-->");
 		temp = temp.next;
 		
 		while(temp != first)
@@ -70,13 +72,77 @@ public class Csll {
 		}
 	}
 	
+	public void InsertNodeAtTheBeginningOfCSLL(int data)
+	{
+		ListNode newNode = new ListNode(data);
+		if(last == null)
+		{
+			last = newNode;
+			newNode.next = last;
+		}
+		newNode.next = last.next;
+		last.next = newNode;
+		
+		length++;
+	}
+	
+	public void InsertNodeAtTheEndOfCSLL(int data)
+	{
+		ListNode newNode = new ListNode(data);
+		if(last == null)
+		{
+			last = newNode;
+			newNode.next = last;
+		}
+		newNode.next = last.next;
+		last.next = newNode;
+		last = newNode;
+		
+		length++;
+	}
+	
+	public void DeleteFirstNodeFromCSLL()
+	{
+		if(IsEmpty())
+		{
+			throw new NoSuchElementException("Circular Singly Linked List is Empty");
+		}
+		ListNode temp = last.next;
+		if(last == temp)
+		{
+			last = null;
+		}
+		else
+		{
+			last.next = temp.next;
+		}
+		temp.next = null;
+		length--;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Csll csll = new Csll();
+		System.out.print("\n\nCreating and printing Circular Linked list ");
 		csll.CreateCircularSinglyLinkedList();
+		csll.displayCSLL();
+		
+		System.out.print("\n\nAdding node at end of Circular Linked list ");
+		csll.InsertNodeAtTheEndOfCSLL(47);
+		csll.displayCSLL();
+		
+		System.out.print("\n\nAdding node at start of Circular Linked list ");
+		csll.InsertNodeAtTheBeginningOfCSLL(-25);
+		csll.displayCSLL();
+		
+		System.out.print("\n\nDeleting first node of Circular Linked list ");
+		csll.DeleteFirstNodeFromCSLL();
+		csll.displayCSLL();
+		
+		System.out.print("\n\nDeleting first node of Circular Linked list ");
+		csll.DeleteFirstNodeFromCSLL();
 		csll.displayCSLL();
 
 	}
-
 }
