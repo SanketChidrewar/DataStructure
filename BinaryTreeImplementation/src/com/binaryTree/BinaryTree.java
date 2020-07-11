9b46e5dc8a9d390fbbb96b38e91da60bace29c14
SanketChidrewar <sanketchidrewar11@gmail.com>
2020-07-11 13:35:06 +0530

    Added code to Traverse Binary Tree in Reccursive and Iterative way by Inorder & in Reccursive way by postorder method

--- BinaryTreeImplementation/src/com/binaryTree/BinaryTree.java
+++ BinaryTreeImplementation/src/com/binaryTree/BinaryTree.java
@@ -1,5 +1,6 @@
 package com.binaryTree;
 
+import java.util.Stack;
 
 public class BinaryTree {
 	
@@ -45,14 +46,85 @@ public class BinaryTree {
 			third.right = seventh;
 		}
 		
-		public void PreOrderTrav(TreeNode root)
+		public void ReccursivePreOrderTrav(TreeNode root)
 		{
 			if(root == null)
 				return;
 			
 			System.out.print(root.data + "  ");
-			PreOrderTrav(root.left);
-			PreOrderTrav(root.right);
+			ReccursivePreOrderTrav(root.left);
+			ReccursivePreOrderTrav(root.right);
+		}
+		
+		public void IterativePreOrderTrav()
+		{
+			if(root == null)
+				return;
+			Stack<TreeNode> stack = new Stack<>();
+			stack.push(root);
+			while(!stack.isEmpty())
+			{
+				TreeNode temp = stack.pop();
+				System.out.print(temp.data + "  ");
+				if(temp.right != null)
+				{
+					stack.push(temp.right);
+				}
+				if(temp.left != null)
+				{
+					stack.push(temp.left);
+				}
+			}
+		}
+		
+		public void reccursiveInOrderTrav(TreeNode root)
+		{
+			if(root == null)
+			{
+				return;
+			}
+			
+			reccursiveInOrderTrav(root.left);
+			System.out.print(root.data + "  ");
+			reccursiveInOrderTrav(root.right);
+
+		}
+		
+		public void IterativeInOrderTrav()
+		{
+			if(root == null)
+				return;
+			Stack<TreeNode> stack = new Stack<>();
+			TreeNode temp = root;
+
+			while(!stack.isEmpty() || temp!=null)
+			{
+				if(temp!=null)
+				{
+					stack.push(temp);
+					temp = temp.left;
+				}
+				else
+				{
+					temp = stack.pop();
+					System.out.print(temp.data + "  ");
+					temp = temp.right;
+				}
+			}
+
+		}
+		
+		public void reccursivePostOrderTrav(TreeNode root)
+		{
+			if(root == null)
+			{
+				return;
+			}
+			
+			reccursivePostOrderTrav(root.left);
+			reccursivePostOrderTrav(root.right);
+			System.out.print(root.data + "  ");
+
 		}
 		
 	public static void main(String[] args) {
@@ -61,8 +133,24 @@ public class BinaryTree {
 		BinaryTree bt = new BinaryTree();
 		bt.createBinaryTree();
 		//System.out.println(bt.root.data+"(root)" + "-->" + bt.root.left.data+"(root left node)");
-		System.out.println("\nPRE-ORDER TRAVERSAL OF TREE :");
-		bt.PreOrderTrav(bt.root);
+		
+		System.out.println("\n Reccursive PRE-ORDER TRAVERSAL OF TREE :");
+		bt.ReccursivePreOrderTrav(bt.root);
+		
+		System.out.println("\n Iterative PRE-ORDER TRAVERSAL OF TREE :");
+		bt.IterativePreOrderTrav();
+		
+		System.out.println("\n Reccursive In-ORDER TRAVERSAL OF TREE :");
+		bt.reccursiveInOrderTrav(bt.root);
+		
+		System.out.println("\n Iterative In-ORDER TRAVERSAL OF TREE :");
+		bt.IterativeInOrderTrav();
+
+		
+		System.out.println("\n Reccursive Post-ORDER TRAVERSAL OF TREE :");
+		bt.reccursivePostOrderTrav(bt.root);
+
+		
 
 	}
 
